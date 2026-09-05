c3f1f5c158a89a0978cd7c3af6d3cfb466f5fb71
Mouna1990 <mmenouer11@gmail.com>
2026-09-05 02:15:10 +0100

    Fix Capacitor app base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-  base: '/crochet-pattern-board/',
+  base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
